fffb93ce6400b99d0932cc9a55bbf83b517167c4
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2025-05-05 19:11:35 -0500

    break the code

--- flow.py
+++ flow.py
@@ -2,6 +2,7 @@ from prefect import flow
 
 @flow(log_prints=True)
 def my_flow():
+    raise Exception("This is a test exception")
     print("Hello again and again and again, world!")
 
 if __name__ == "__main__":
